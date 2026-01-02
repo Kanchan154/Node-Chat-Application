7fa4341e3e30c6c7440b6dc8256bd2726d0ae3e0
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-01-02 22:26:04 +0530

    Initializing and setting up the backend

---
+++ Backend/Utils/GenerateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generatetikenandSetCookie = (userID, res) => {
+    const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+        expiresIn: '15d'
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000, //ms
+        httpOnly: true, //prevent XSS attacks cross-site scrypting attaks
+        sameSite: "strict", //CSRF attacks cross-site requests forgrey attacks
+        secure : process.env.NODE_ENV !== "development",
+    });
+}
+
+export default generatetikenandSetCookie;
