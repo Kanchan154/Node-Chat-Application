321fe5437da858515f042ac02f982985cac7149d
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-01-04 22:10:49 +0530

    Fix formatting and remove extra newlines in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,14 +17,12 @@ function App() {
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
       <Route path='/' element= {authUser ? <Home/> : <Navigate to={"/login"}/>}/>
-      <Route path='/login' element= {authUser ? <Navigate to={'/'}/> : <Login/>}/>
-      <Route path='/signup' element= {authUser ? <Navigate to={'/'}/> : <Signup/>}/>
-      
+        <Route path='/login' element= {authUser ? <Navigate to={'/'}/> : <Login/>}/>
+        <Route path='/signup' element= {authUser ? <Navigate to={'/'}/> : <Signup/>}/>
       </Routes>
-
       <Toaster/>
     </div>
   )
 }
 
-export default App
+export default App;
