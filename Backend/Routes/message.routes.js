55ed3b0132c6ab996e6d24b202e87d9c0250dc4a
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-01-28 23:11:10 +0530

    Refactor message routes for consistency and clarity

--- Backend/Routes/message.routes.js
+++ Backend/Routes/message.routes.js
@@ -2,9 +2,10 @@ import express from "express";
 import { sendMessage, getMessage } from "../Controller/message.controller.js";
 import protectRoute from "../Middleware/protextroute.js";
 
-const router = express.Router()
-
-router.post("/send/:id", protectRoute, sendMessage)
+const router = express.Router();
+// send message route
+router.post("/send/:id", protectRoute, sendMessage);
+// get message route
 router.get('/:id', protectRoute, getMessage)
 
 export default router;
